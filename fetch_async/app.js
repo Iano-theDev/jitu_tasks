6b175804e72194d985f9ca3f3384e32466299650
Iano-theDev <sinceianmike@gmail.com>
2023-01-19 17:15:21 +0300

    Add fetch api

--- fetch_async/app.js
+++ fetch_async/app.js
@@ -1,2 +1,33 @@
-fetch('https://requres.in/api/users')
-    .then(res => console.log(res))
+// fetch('https://jsonplaceholder.typicode.com/todos/'), {
+//     method: 'POST',
+//     body: JSON.stringify({
+//         name: 'title'
+//     })
+// }
+//     .then(res => {
+//         if (res.ok) {
+//             console.log('SUCCESS!')
+//         } else {
+//             console.log("Not susccessful")
+//         }
+//     })
+//     .then(data => console.log(data))
+//     .catch(error => console.log('ERROR'))
+
+
+fetch ('https://jsonplaceholder.typicode.com/todos', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        userId: 'userId',
+        id: "id",
+        title: "title",
+        completed: "completed"
+    })
+}).then(res => {
+    return res.json()
+})
+.then(data => console.log(data))
+.catch(error => console.log('ERROR'))
