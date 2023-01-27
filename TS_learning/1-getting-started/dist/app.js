1f31308b5e0e2401350553c46700faad5707289a
Iano-theDev <sinceianmike@gmail.com>
2023-01-27 21:45:43 +0300

    Add modifications

--- TS_learning/1-getting-started/dist/app.js
+++ TS_learning/1-getting-started/dist/app.js
@@ -1,38 +1,34 @@
+"use strict";
 //
-let ids: number[] = [1, 1, 3,]
-let arr: any[] = [1, "s", true]
-
+let ids = [1, 1, 3,];
+let arr = [1, "s", true];
 //tuple
 //follows the particular order
-let person: [number, string, boolean] = [1, "Hello", true];
-
+let person = [1, "Hello", true];
 //Tupule array
-let employee: [number | string, string | boolean][]
+let employee;
 employee = [
     [1, 'yano'],
     [2, 'john'],
     [3, true],
     ["yanos", false]
-]
-
+];
 // Union
-let id: string | number
-id = '23'
-
+let id;
+id = '23';
 //enum
-enum Direction1 {
-    up,
-    Down,
-    Left,
-    Right,
-}
-
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["up"] = 0] = "up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
 class Enum {
-    constructor(obj: object) {
+    constructor(obj) {
         Object.assign(this, obj);
     }
 }
-
 // const DaysOfWeek = new Enum({
 //     SUNDAY: 0,
 //     MODAY: 1,
@@ -42,64 +38,22 @@ class Enum {
 //     FRIDAY: 5,
 //     SATURDAY: 6,
 // });
-
 // console.log(DaysOfWeek.SUNDAY)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // let message: string = 'hello world';
-
 // let heading = document.createElement('h1');
-
 // heading.textContent = message;
-
 // document.body.appendChild(heading);
-
 // let box;
-
 // console.log(typeof(box));
-
 // box = "hello";
 // console.log(typeof(box));
-
 // box = 4;
 // console.log(typeof(box));
-
 // interface Product{
 //     id: number,
 //     name: string,
 //     price: number
 // };
-
 // function getProduct(id : number) : Product{
 //     return {
 //         id: id,
@@ -107,15 +61,11 @@ class Enum {
 //         price: 99.5
 //     }
 // }
-
 // const showProduct = (name: string, price: number) => {
 //     console.log(`The product ${name} costs ${price}$.`);
 // };
-
 // const product = getProduct(1);
 // console.log(`The product ${product.name} costs $${product.price}`);
-
-
 // type manipulation 
 // type Department={
 //     hod: string
@@ -123,31 +73,23 @@ class Enum {
 //     name:string
 //     records: object[]
 // }
-
 // let IT:Department={
 //     hod:'Mr Elvis',
 //     worker:['John', 'doe'],
 //     name: "THE It Department",
 //     records: [{}]
 // }
-
 // type KeyOptions = keyof Department;
-
 // let xp: KeyOptions= "records"
-
-
 // for (let keys in IT){
 //     console.log(keys);
 //     //let keyOptions: 'hod
 // }
-
 // //TS types
 // const mainBody: any = document.querySelector('.main')
 // const heading = document.querySelector('h1')
 // const myDiv = document.createElement('div');
 // myDiv.innerHTML = `<p>Yooooooooo there, im a div thats being appended to the main<p>`
-
 // const appendMain = mainBody.appendChild(myDiv)
-
-
 // //
+//# sourceMappingURL=app.js.map
